7558f7902a57f1d8e42c230ccc0fae622ef414d9
David Pitt <dpitt@keyholesoftware.com>
2012-04-27 18:53:44 -0500

    settings object

---
+++ src/main/java/com/khs/sherpa/servlet/Settings.java
@@ -0,0 +1,13 @@
+package com.khs.sherpa.servlet;
+
+import static com.khs.sherpa.util.Defaults.*;
+
+public class Settings {
+	
+	public String dateFormat = DATE_FORMAT;
+	public String timeFormat = TIME_FORMAT;
+	public long sessionTimeout = SESSION_TIMEOUT;
+	public boolean endPointAuthentication = ENDPOINT_AUTHENTICATION;
+	public String endpointPackage = null;
+	
+}
