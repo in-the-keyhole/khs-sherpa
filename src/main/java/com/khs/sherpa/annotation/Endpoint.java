324a083e6e8e1c93c61d1c306146a56790a37d72
Mark Determan <mdeterman@gmail.com>
2012-05-24 12:12:47 -0500

    make sure @Endpoint is target Type

--- src/main/java/com/khs/sherpa/annotation/Endpoint.java
+++ src/main/java/com/khs/sherpa/annotation/Endpoint.java
@@ -16,9 +16,12 @@ package com.khs.sherpa.annotation;
  * limitations under the License.
  */
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Endpoint {
 
