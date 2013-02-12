be3ba08816c2c75b33f23870fdbf4178710d3b2c
Mark Determan <mdeterman@gmail.com>
2013-02-12 07:17:21 -0600

    add new exceptions

---
+++ src/main/java/com/khs/sherpa/exception/SherpaSessionTimeoutException.java
@@ -0,0 +1,7 @@
+package com.khs.sherpa.exception;
+
+public class SherpaSessionTimeoutException extends SherpaRuntimeException {
+
+	private static final long serialVersionUID = -5613607550320570215L;
+
+}
